418a91589d2fad09ae5a7b0ca8dcd4c13d7c06a2
foo7print <foo7print@gmail.com>
2024-10-23 15:38:02 +0900

    手番の処理

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,21 @@ function Square({ value, onSquareClick }: { value: string | null, onSquareClick:
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
 
   function handleClick(i: number) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
