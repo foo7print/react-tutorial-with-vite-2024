37e09d4643aaf869421160805ec164df8b314114
foo7print <foo7print@gmail.com>
2024-10-24 11:40:49 +0900

    最後のお掃除

--- src/App.tsx
+++ src/App.tsx
@@ -53,21 +53,19 @@ function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean, squares: Array<
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 == 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: Array<string | null>) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 == 0);
   }
 
   const moves = history.map((_squares, move) => {
